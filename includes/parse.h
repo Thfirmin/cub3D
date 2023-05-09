466ce1d1fd26f4daab2046b8f29f1f6dd71d4381
vitorsantanna2 <95882160+vitorsantanna2@users.noreply.github.com>
2023-05-09 00:35:10 -0300

    Updated header to parse

--- includes/parse.h
+++ includes/parse.h
@@ -6,7 +6,7 @@
 /*   By: jsantann <jsantann@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 23:19:24 by jsantann          #+#    #+#             */
-/*   Updated: 2023/05/09 00:20:24 by jsantann         ###   ########.fr       */
+/*   Updated: 2023/05/09 00:34:18 by jsantann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 
 # include "cub3d.h"
 
-void	error_menu(char **argv, int argc);
+int		error_menu(char **argv, int argc);
 void	error_argc(int argc);
-void	error_permission(char *argv);
+int		error_permission(char *argv);
 void	error_filename(char *argv);
 
 #endif
