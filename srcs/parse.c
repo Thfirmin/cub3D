c6f7cbdacb7961ca77a8a8ae213ecb0866901505
vitorsantanna2 <95882160+vitorsantanna2@users.noreply.github.com>
2023-05-09 00:10:42 -0300

    Parse on norminette

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: jsantann <jsantann@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 23:26:06 by jsantann          #+#    #+#             */
-/*   Updated: 2023/05/09 00:07:02 by jsantann         ###   ########.fr       */
+/*   Updated: 2023/05/09 00:10:10 by jsantann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ void	error_menu(char **argv, int argc)
 	error_filename(argv[1]);
 }
 
-void	error_argc(int	argc)
+void	error_argc(int argc)
 {
 	if (argc != 2)
 	{
 		ft_putstr_fd("Error\n", 2);
-		ft_putstr_fd("The game must be initialized ", 2); 
+		ft_putstr_fd("The game must be initialized ", 2);
 		ft_putstr_fd("with the following arguments:\n", 2);
 		ft_putstr_fd("./cub3d filename.cub\n", 2);
 		exit(0);
