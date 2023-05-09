2dec918ddaab88b6d1e4dd1704d71d7798178acc
vitorsantanna2 <95882160+vitorsantanna2@users.noreply.github.com>
2023-05-09 00:04:51 -0300

    Updated cub3d.c

--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -6,15 +6,16 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 02:03:12 by thfirmin          #+#    #+#             */
-/*   Updated: 2023/05/07 02:06:38 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/05/08 23:44:07 by jsantann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+#include "parse.h"
 
 int	main(int argc, char **argv)
 {
-	(void) argc;
+	error_menu(argv, argc);
 	(void) argv;
 	return (0);
 }
