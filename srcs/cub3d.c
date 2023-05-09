fb2e6507af497554085ae6b3324b39445b914a2f
vitorsantanna2 <95882160+vitorsantanna2@users.noreply.github.com>
2023-05-09 01:04:32 -0300

    Removed void(argv)

--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 02:03:12 by thfirmin          #+#    #+#             */
-/*   Updated: 2023/05/09 00:33:02 by jsantann         ###   ########.fr       */
+/*   Updated: 2023/05/09 01:04:16 by jsantann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,5 @@ int	main(int argc, char **argv)
 	int	fd;
 
 	fd = error_menu(argv, argc);
-	(void) argv;
 	return (0);
 }
