50d4ece4ba41ae204fa76b29c85e13361244ab88
vitorsantanna2 <95882160+vitorsantanna2@users.noreply.github.com>
2023-05-09 00:12:05 -0300

    Header on norminette

--- includes/parse.h
+++ includes/parse.h
@@ -6,14 +6,14 @@
 /*   By: jsantann <jsantann@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 23:19:24 by jsantann          #+#    #+#             */
-/*   Updated: 2023/05/08 23:46:05 by jsantann         ###   ########.fr       */
+/*   Updated: 2023/05/09 00:11:51 by jsantann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSE_H
 # define PARSE_H
 
-#include "cub3d.h"
+# include "cub3d.h"
 
 void	error_menu(char **argv, int argc);
 void	error_argc(int argc);
